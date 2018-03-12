42377b6e14af3587094e13b5be4dab3064104899
David Storey <david.storey@realise.com>
2018-03-12 13:11:22 +0000

    Boilerplate setup

---
+++ gulp-tasks/scss-lint.js
@@ -0,0 +1,11 @@
+module.exports = function (gulp, plugins, config, errorHandler) {
+	gulp.task('scss-lint', function() {
+		return gulp.src(config.lintedFiles)
+			.pipe(plugins.cached('scss-lint'))
+			.pipe(plugins.postcss([
+				plugins.stylelint(),
+
+				plugins.postcssReporter({ clearReportedMessages: true, throwError: false, noIcon: false, plugins: ['stylelint'] })
+			], { syntax: plugins.postcssScss }));
+	});
+};
---
+++ gulp-tasks/watch.js
@@ -0,0 +1,14 @@
+module.exports = function (gulp, plugins, config, errorHandler) {
+	gulp.task('watch', function() {
+		gulp.watch(config.paths.input.staticDev,  ['copy-dev']);
+		gulp.watch(config.paths.input.styles, ['sass-develop']);
+		gulp.watch(config.paths.input.spriteSrc, ['sprite-create']);
+		gulp.watch(config.paths.input.images + '**/*.svg', ['svg2png']);
+		gulp.watch(config.paths.input.scripts + '**/*.js', function () {
+			plugins.runSequence(['es-lint', 'babelify-develop'], 'browser-reload');
+		});
+		gulp.watch(config.paths.input.htmlWatch, function () {
+			plugins.runSequence('html-templating-develop', 'html-lint', 'browser-reload');
+		});
+	});
+};
