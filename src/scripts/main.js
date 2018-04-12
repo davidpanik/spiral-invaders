5ab6a4eac79985975a6758457c712b6af79f55a5
David Storey <david.storey@realise.com>
2018-04-12 16:40:08 +0100

    Added an enemy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@
 
 import Phaser from './vendor/phaser.min';
 
-let cursors, ship;
+let cursors, ship, alien;
 const missiles = {
 	canFire: true,
 	delay: 500,
@@ -27,12 +27,15 @@ const config = {
 		preload: function() {
 			this.load.image('space', './images/space.png');
 			this.load.spritesheet('ship', './images/ship.png', { frameWidth: 32, frameHeight: 44 });
+			this.load.spritesheet('alien', './images/alien.png', { frameWidth: 32, frameHeight: 32 });
 			this.load.spritesheet('missile', './images/missile.png', { frameWidth: 8, frameHeight: 19 });
 		},
 		create: function () {
 			this.add.image(WIDTH / 2, HEIGHT / 2, 'space');
 
-			ship = this.physics.add.sprite(400, 550, 'ship');
+			ship = this.physics.add.sprite(WIDTH / 2, HEIGHT - 50, 'ship');
+			alien = this.physics.add.sprite(WIDTH / 2, HEIGHT / 2, 'alien');
+
 			this.anims.create({
 				key: 'ship-default',
 				frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 1 }),
@@ -52,15 +55,43 @@ const config = {
 				repeat: -1
 			});
 			this.anims.create({
+				key: 'alien-default',
+				frames: this.anims.generateFrameNumbers('alien', { start: 0, end: 1 }),
+				frameRate: 10,
+				repeat: -1
+			});
+			this.anims.create({
+				key: 'alien-left',
+				frames: this.anims.generateFrameNumbers('alien', { start: 2, end: 3 }),
+				frameRate: 10,
+				repeat: -1
+			});
+			this.anims.create({
+				key: 'alien-right',
+				frames: this.anims.generateFrameNumbers('alien', { start: 4, end: 5 }),
+				frameRate: 10,
+				repeat: -1
+			});
+			this.anims.create({
 				key: 'missile-default',
 				frames: this.anims.generateFrameNumbers('missile', { start: 0, end: 1 }),
 				frameRate: 10,
 				repeat: -1
 			});
 
+			this.physics.world.on("worldbounds", function (body) {
+				console.log('crash', body);
+			});
+
 			ship.setCollideWorldBounds(true);
 			ship.depth = 10;
 			ship.anims.play('ship-default');
+
+			alien.anims.play('alien-default');
+			alien.depth = 20;
+			alien.angle = 180;
+			alien.setVelocityX(-80);
+			alien.anims.play('alien-right', true);
 
 			cursors = this.input.keyboard.createCursorKeys();
 		},
@@ -81,6 +112,12 @@ const config = {
 					let missile = this.physics.add.sprite(ship.x, ship.y, 'missile');
 					missile.anims.play('missile-default');
 					missile.setVelocityY(-200);
+
+					setTimeout(() => {
+						if (missile.body.checkWorldBounds()) {
+							missile.destroy();
+						}
+					}, 4000);
 					
 					missiles.canFire = false;
 					missiles.timer = setTimeout(() => {
